01ce8872d5cc4d349eeacea6dfb21610d9c5e963
Ujjwal Kandel <ujjwalkandel1@gmai.com>
2025-04-10 17:01:24 +0545

    Es6 javascript

---
+++ js_adv/mathOperationC.js
@@ -0,0 +1,21 @@
+//named export
+
+function add(a, b) {
+    return a + b
+}
+
+function subtract(a, b) {
+    return a - b
+}
+
+function multiply(a, b) {
+    return a * b
+}
+
+module.exports = {
+    add,
+    subtract,
+    multiply
+
+}
+
