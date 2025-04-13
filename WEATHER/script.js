b5001201dcc3228a10de5aae2a45cabf8f073fc0
Ujjwal Kandel <ujjwalkandel1@gmai.com>
2025-04-13 13:14:36 +0545

    weather update

--- WEATHER/script.js
+++ WEATHER/script.js
@@ -9,15 +9,27 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const API_KEY = "5f56d525d1619d0a2cd2eac4ce55588e"
 
-    getWeatherBtn.addEventListener('click', () => {
+    getWeatherBtn.addEventListener('click', async () => {
         const city = cityInput.ariaValueMax.trim()
         if(!city) return;
-    })
 
-    function fetchWeatherData(city){
+        try{
+            const weatherData = await fetchWeatherData(city);
+            displayWeatherData(weatherData);
+        } catch (error){
+            showError();
+        }
+    });
+
+    async function fetchWeatherData(city){
         //gets the data
-    }
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`;
 
+        const response = fetch(url);
+        console.log(typeof response);
+        console.log("RESPONSE", response);
+    }
+//13 min
     function displayWeatherData(weatherData){
         //display
     }
