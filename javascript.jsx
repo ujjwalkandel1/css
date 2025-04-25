f7039cf74d96fbd690c7f357db43b7adeba01bf8
Ujjwal Kandel <ujjwalkandel1@gmai.com>
2025-04-25 17:23:25 +0545

    react

---
+++ javascript.jsx
@@ -0,0 +1,62 @@
+// JavaScript You need to know for react
+
+// export default function DoSomething() {
+
+// }
+
+// export const DoSomething = () => {}
+
+
+// Ternary operators
+// let age = 10;
+// let name = "pedro";
+
+// //if else statement
+// if (age > 10) {
+//     name = "pedro"
+// }
+// else {
+//     name="Jack"
+// }
+
+// let age = 16;
+// let name = age > 10 ? "Pedro" : "Jack";
+
+// const Component = () => {
+//     return age > 10 ? <div>Ujjwal</div> : <div>Jack</div>
+// }
+
+// const person = {
+//     name: "ujjwal",
+//     age: 23 ,
+//     isMarried: false,
+
+// };
+
+// const name = person.name
+// const age = person.age
+// const isMarried = person.isMarried
+
+// const { name, age, IsMarried} = person;
+
+// const name = "ujjwal";
+// const age = 20;
+
+// const person = {
+//     name,
+//     age,
+//     isMarried: false,
+// }
+
+// const person = {
+//     name: "ujjwal",
+//     age: 23 ,
+//     isMarried: false,
+
+// };
+
+// const person2 = {...person, name:"jack" };
+
+// const names = ["Pedro", "jack", "jessica" ];
+// const names2 = [...names, "joel"];   
+
